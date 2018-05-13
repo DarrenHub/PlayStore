5b406decf2844dd3f60f68b77a6c35207c9b8ba9
Jing <lyovse@gmail.com>
2018-05-13 17:32:53 +1000

    Update Content, Magazine, and PlayStore classes

--- src/Content.java
+++ src/Content.java
@@ -1,16 +1,33 @@
+import java.util.ArrayList;
 
 public abstract class Content {
-	private String id;
-	private String name;
-	private double price;
+	private String id; // Content ID
+	private String name; // Content name
+	private double price; // Content price
 	private int numDownloads; // Number of downloads
-	// private Array review....
+	private ArrayList<Comment> review;
 
 	public Content(String id, String name, double price) {
 		this.id = id;
 		this.name = name;
 		this.price = price;
 		this.numDownloads = 0;
-		// this.review....
+		this.review = new ArrayList<Comment>();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getNumDownloads() {
+		return numDownloads;
 	}
 }
--- src/Magazine.java
+++ src/Magazine.java
@@ -1,12 +1,13 @@
 
-public abstract class Magazine extends Reading {
+public class Magazine extends Reading {
 	private String titleMain; // Title of the main feature
-	
-	public Magazine(String id, String name, double price, String publisher, String genre, int numPages, String titleMain) {
+
+	public Magazine(String id, String name, double price, String publisher, String genre, int numPages,
+			String titleMain) {
 		super(id, name, price, publisher, genre, numPages);
 		this.titleMain = titleMain;
 	}
-	
+
 	public Magazine(String id, String name, String publisher, String genre, int numPages, String titleMain) {
 		super(id, name, publisher, genre, numPages);
 		this.titleMain = titleMain;
