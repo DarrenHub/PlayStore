d1359912f3658efc6c08389fab5d44e1192c782d
Jing <lyovse@gmail.com>
2018-05-15 12:47:18 +1000

    Fix a bug where author array wasn't initialized

--- src/Book.java
+++ src/Book.java
@@ -11,6 +11,7 @@ public class Book extends Reading {
 	public Book(String id, String name, String publisher, String genre, int numPages, String[] author) {
 		super(id, name, publisher, genre, numPages); // Default price to 0 (free) when price is not passed as an
 														// argument
+		this.author = new String[author.length];
 		System.arraycopy(author, 0, this.author, 0, author.length);
 	}
 }
--- src/PlayStore.java
+++ src/PlayStore.java
@@ -100,10 +100,12 @@ public class PlayStore {
 		String[] authors3 = { "Thomas H. Cormen", "Charles E. Leiserson", "Ronald L. Rivest", "Clifford Stein" };
 		Book b3 = new Book("R3", "Introduction to algorithms", 100, "MIT Press", "Computer Science", 1312, authors3);
 		Magazine m1 = new Magazine("R4", "Forbes", 8, "Forbes Media‎", "Business", 50, "World’s richest under 30");
+		Book b4 = new Book("R4", "The Grand Design", "Charles Scribner's Sons", "Novel", 180, authors2);
 
 		admin.addContents(b1);
 		admin.addContents(b2);
 		admin.addContents(b3);
+		admin.addContents(b4);
 		admin.addContents(m1);
 
 		// adding new games
